6dbfe14ac7a2f3f07579950a75b2df48833dbe91
Matheus da Silva Ferreira <48540484+math-ferreira@users.noreply.github.com>
2023-03-30 22:38:33 -0300

    Update TwoStrings.java

--- src/interview/preparation/kit/dictionaries/and/hashmaps/TwoStrings.java
+++ src/interview/preparation/kit/dictionaries/and/hashmaps/TwoStrings.java
@@ -1,4 +1,72 @@
 package interview.preparation.kit.dictionaries.and.hashmaps;
 
-public class TwoStrings {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class TwoStrings {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+
+        String theyHaveSameValue = "NO";
+
+        int[] s1Array = s1.chars().sorted().distinct().toArray();
+        int[] s2Array = s2.chars().sorted().distinct().toArray();
+
+        for (int s1Int : s1Array) {
+            for (int s2Int : s2Array) {
+                if (s1Int == s2Int) {
+                    theyHaveSameValue = "YES";
+                    break;
+                }
+            }
+        }
+        return theyHaveSameValue;
+    }
+
+}
+
+class TwoStringsSolution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = TwoStrings.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
